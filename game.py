218a1028ae850f2a7112bd7620e61db347e28bbc
Aiden <aylatif182@gmail.com>
2026-08-07 20:50:29 -0400

    added a death screen and made the snake snape to the grid

--- game.py
+++ game.py
@@ -14,7 +14,9 @@ pygame.display.set_caption(f"Snake Game - Score: 0")
 
 clock = pygame.time.Clock()
 
-Segment_gap = 8
+Segment_gap = 11
+
+Lost = False
 
 def draw_grid(screen):
     color_a = (162, 209, 73)   # light green
@@ -33,29 +35,98 @@ class Piece:
         self.radius = radius
         self.color = color
         self.speed = speed
+        self.dir_x = 0
+        self.dir_y = 0
 
         self.segments = []
         self.history = deque(maxlen=(Segment_gap *200))
+        self.get_all_grid_center_points()
 
+    def get_all_grid_center_points(self):
+        self.points_y = []
+        for row in range(GRID_HEIGHT):
+            for col in range(GRID_WIDTH):
+                y = row * CELL_SIZE + CELL_SIZE // 2
+                self.points_y.append((y))
+        
+        self.points_x = []
+        for row in range(GRID_HEIGHT):
+            for col in range(GRID_WIDTH):
+                x = col * CELL_SIZE + CELL_SIZE // 2
+                self.points_x.append((x))
     
     def move(self, keys):
-        if keys[pygame.K_w]:
-            self.pos_y -= self.speed
-        if keys[pygame.K_s]:
-            self.pos_y += self.speed
-        if keys[pygame.K_a]:
-            self.pos_x -= self.speed
-        if keys[pygame.K_d]:
-            self.pos_x += self.speed
+        if keys[pygame.K_w] and self.dir_y == 0:
+            self.dir_x, self.dir_y = 0,-1
+            bestsofar = float('inf')
+            best_point = None
+            for point in self.points_x:
+                distance = abs(point - self.pos_x)
+                if distance < bestsofar:
+                    bestsofar = distance
+                    best_point = point
+                else:
+                    pass
+            self.pos_x = best_point
+        if keys[pygame.K_s] and self.dir_y == 0:
+            self.dir_x, self.dir_y = 0,1
+            bestsofar = float('inf')
+            best_point = None
+            for point in self.points_x:
+                distance = abs(point - self.pos_x)
+                if distance < bestsofar:
+                    bestsofar = distance
+                    best_point = point
+                else:
+                    pass
+            self.pos_x = best_point
+        if keys[pygame.K_a] and self.dir_x == 0:
+            self.dir_x, self.dir_y = -1,0
+            bestsofar = float('inf')
+            best_point = None
+            for point in self.points_y:
+                distance = abs(point - self.pos_y)
+                if distance < bestsofar:
+                    bestsofar = distance
+                    best_point = point
+                else:
+                    pass
+            self.pos_y = best_point
+        if keys[pygame.K_d] and self.dir_x == 0:
+            self.dir_x, self.dir_y = 1,0
+            bestsofar = float('inf')
+            best_point = None
+            for point in self.points_y:
+                distance = abs(point - self.pos_y)
+                if distance < bestsofar:
+                    bestsofar = distance
+                    best_point = point
+                else:
+                    pass
+            self.pos_y = best_point
+        
+        self.loseing()
+
+        self.pos_x += self.dir_x * self.speed
+        self.pos_y += self.dir_y * self.speed
 
         self.history.appendleft((self.pos_x, self.pos_y))
 
         for i in range(len(self.segments)):
-            hist_index = (i +1) * 8
+            hist_index = (i +1) * Segment_gap
             if hist_index < len(self.history):
                 self.segments[i] = self.history[hist_index]
             elif self.segments:
                 pass
+
+    def loseing(self):
+            global Lost
+            for seg_x, seg_y in self.segments[1:-1]:
+                dx = self.pos_x - seg_x
+                dy = self.pos_y - seg_y
+                distance = (dx ** 2 + dy ** 2) ** 0.5
+                if distance < self.radius + self.radius:
+                    Lost = True
 
     def grow(self):
         if self.segments:
@@ -85,8 +156,8 @@ class Apple:
         self.pos_ygrow = self.pos_y + 24
         pygame.draw.circle(screen, self.color, (self.pos_xgrow, self.pos_ygrow), self.radius)
 
-Piece_draw = Piece(320, 240, 25,(0, 200, 255))
-Apple_spawn = Apple(25,(255, 0, 0))
+Piece_draw = Piece(408, 360, 23,(0, 200, 255))
+Apple_spawn = Apple(23,(255, 0, 0))
 
 runing = True
 while runing:
@@ -110,6 +181,18 @@ while runing:
         Piece_draw.grow()
         player_score += 1
         pygame.display.set_caption(f"Snake Game - Score: {player_score}")
+    if Lost == True:
+        overlay = pygame.Surface((screen.get_width(), screen.get_height()))
+        overlay.set_alpha(180)
+        overlay.fill((30, 30, 30))
+        screen.blit(overlay, (0, 0))
+
+        font = pygame.font.Font(None, 74)
+        text_surface = font.render(f"Game Over - Score:{player_score}", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+        screen.blit(text_surface, text_rect)
+        Apple_spawn.dir_x = 0
+        Apple_spawn.dir_y = 0
     pygame.display.flip()
     clock.tick(60)
 
